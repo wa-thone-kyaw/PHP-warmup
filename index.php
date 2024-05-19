f6c6e0b27fbb7ff035f9151d80d2131edfb4b10f
wa-thone-kyaw <wathonekyaw.x@gmail.com>
2024-05-19 07:59:16 +0630

    variable

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Demo</title>
+  <style>
+body {
+
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  margin:0; 
+  font-family: sans-serif;
+}
+
+
+  </style>
+  </head>
+  <body>
+
+  <?php
+
+  $name = "Dark Matter";
+  ?>
+    <h1>
+    You have read"<?php echo $name;?>"
+    </h1>
+  </body>
+</html>
